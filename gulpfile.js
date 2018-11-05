7c8f3642692073a789ad07a79d01e44875bd040d
smihaylov <samuil.mihaylov@dreamix.eu>
2018-11-05 22:56:32 +0300

    Gulp tasks refactored according to the new package version gulp@4.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,16 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const rename = require("gulp-rename");
 
-gulp.task('build', ['prepare-js', 'prepare-css']);
-
 gulp.task('prepare-js', function () {
-  gulp.src(['target/webapp/*.js'])
+  return gulp.src(['target/webapp/*.js'])
     .pipe(concat('angular.js'))
     .pipe(gulp.dest('src/main/webapp/dist'));
 });
 
 gulp.task('prepare-css', function () {
-  gulp.src(['target/webapp/styles.*.css'])
+  return gulp.src(['target/webapp/styles.*.css'])
     .pipe(rename('styles.css'))
     .pipe(gulp.dest('src/main/webapp/dist'));
 });
 
+gulp.task('build', gulp.parallel('prepare-js', 'prepare-css'));
